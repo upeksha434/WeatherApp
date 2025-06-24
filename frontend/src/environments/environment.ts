ece0a4c9091d618cdca4b7dce98a33242fe8e322
upeksha434 <upeksha434@gmail.com>
2025-06-24 19:32:45 +0530

    css changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Development environment
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/weather'
+  apiUrl: 'https://weatherapp-d12u.onrender.com/api/weather'
 };
